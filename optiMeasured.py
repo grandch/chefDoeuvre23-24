1ad09d9bbfda420507c6a43659cf768aefcb6fdb
grandch <charliegrand31@gmail.com>
2024-02-28 20:38:09 +0100

    measured data

--- optiMeasured.py
+++ optiMeasured.py
@@ -1,213 +1,86 @@
 import mitsuba as mi
-import drjit as dr
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
-import xml.etree.ElementTree as ET
+import os
+import rawpy
 
 mi.set_variant('llvm_ad_rgb')
 
-
 # parsing prg args
 parser = argparse.ArgumentParser()
-parser.add_argument("scene_path", help="path to the scene to render", type=str)
-parser.add_argument("ref_path", help="path to ref images", type=str)
-parser.add_argument("-x", help="x resolution, default is 720", type=int)
-parser.add_argument("-y", help="y_resolution, default is 480", type=int)
-parser.add_argument("-s", help="number of samples for the renders, default is 8", type=int)
-parser.add_argument("-fs", help="number of samples for the final renders, default is 4096", type=int)
-parser.add_argument("-lr", help="learning rate, default is 0.02", type=float)
-parser.add_argument("-it", help="iteration count, default is 40", type=int)
-parser.add_argument("-init_sigma_t", help="init value of sigma_t parameter, default is 0.002", type=float)
+parser.add_argument("-s", help="number of samples for the renders, default is 128", type=int)
 args = parser.parse_args()
 
-spp, xRes, yRes, lr, iteration_count, fspp, init_sigma_t = None, None, None, None, None, None, None
+ref_sample = None
 if args.s != None:
-	spp = args.s
+	ref_sample = args.s
 else:
-	spp = 8
-if args.x != None:
-	xRes = args.x
-else:
-	xRes = 720
-if args.y != None:
-	yRes = args.y
-else:
-	yRes = 720
-if args.lr != None:
-    lr = args.lr
-else:
-    lr = 0.02
-if args.it != None:
-    iteration_count = args.it
-else:
-    iteration_count = 40
-if args.fs != None:
-    fspp = args.fs
-else:
-    fspp = 4096
-if args.init_sigma_t != None:
-    init_sigma_t = args.init_sigma_t
-else:
-    init_sigma_t = 0.002
+	ref_sample = 128
 
+scene = mi.load_file("scenes/optimizationScene_measured.xml")
 
-def img_diff(img1, img2):
-    # TODO : check if same size
-    npimg1 = np.array(img1)
-    npimg2 = np.array(img2)
-    diff = np.empty((np.shape(npimg1)[0], np.shape(npimg1)[1]), dtype=float)
-    for i in range(np.shape(npimg1)[0]):
-        for j in range(np.shape(npimg1)[1]):
-            diff[i][j] = np.linalg.norm(npimg1[i][j] - npimg2[i][j])
-    return mi.Bitmap(diff)
+def load_sensors_lights_and_refs(sensors_dir):
+    origin, target, up, laser_dir = [], [], [], []
+    dirs = sorted(os.listdir(sensors_dir))
+    ref = {}
+	
+    for dir in dirs:
+        lambdas = os.listdir(sensors_dir + "/" + dir)
+        for lam in lambdas:
+            if ref.__contains__(lam):
+                ref[lam].append(rawpy.imread(sensors_dir + "/" + dir + "/" + lam + "/" + sorted(os.listdir(sensors_dir + "/" + dir + "/" + lam))[0]))
+            else:
+                ref[lam] = []
+                ref[lam].append(rawpy.imread(sensors_dir + "/" + dir + "/" + lam + "/" + sorted(os.listdir(sensors_dir + "/" + dir + "/" + lam))[0]))
 
-path_to_ref = args.ref_path
-path_to_sensor = args.sensor_path
+    # origin.append([0.4124579436349758*10, 0.058085583954402234*10, -0.33230771606359427*10])
+    # origin.append([-0.26187965706737165*10, 0.0639193905312978*10, -0.3371195109510675*10])
+    # origin.append([0.07629510952287223*10, 0.06160241046937788*10, -0.4743865210065597*10])
+    # origin.append([-0.04723309843906157*10, 0.06267489306727239*10, -0.45902214575013506*10])
+    # origin.append([0.1995977351808692*10, 0.06094895876798736*10, -0.4572594416582021*10])
+                
+    origin.append([0.4124579436349758*5, 0.058085583954402234*5, -0.33230771606359427*5])
+    origin.append([-0.26187965706737165*5, 0.0639193905312978*5, -0.3371195109510675*5])
+    origin.append([0.07629510952287223*5, 0.06160241046937788*5, -0.4743865210065597*5])
+    origin.append([-0.04723309843906157*5, 0.06267489306727239*5, -0.45902214575013506*5])
+    origin.append([0.1995977351808692*5, 0.06094895876798736*5, -0.4572594416582021*5])
 
-def load_ref_and_sensors(sensors_file):
-    origin, target, up = [], [], []
-    tree = ET.parse(sensors_file)
-    capteurs = tree.getroot()
-    for capteur in capteurs:
-        o, t, u = [], [], []
-        for v_o in capteur.iter('origin'):
-            o.append(np.float64(v_o.text))
-        for v_t in capteur.iter('target'):
-            t.append(np.float64(v_t.text))
-        for v_u in capteur.iter('up'):
-            u.append(np.float64(v_u.text))
-        origin.append(o)
-        target.append(t)
-        up.append(u)
+    # target.append([1.11938331,  0.05305775, -1.039578])
+    # target.append([-0.96766999,  0.08533974, -1.04521638])
+    # target.append([0.07639533,  0.06385106, -1.47438399])
+    # target.append([-0.3058195,   0.07056089, -1.42497809])
+    # target.append([0.45838115,  0.06012653, -1.42319447])
 
-    origin, target, up = np.array(origin), np.array(target), np.array(up)
-    return origin, target, up
+    for i in range(5):
+        target.append([0,0,0])
 
-origin, target, up = load_sensors(path_to_sensor)
-sensor_count = len(origin)
-sensors = []
+    up.append([0,1,0])
+    up.append([0,1,0])
+    up.append([0,1,0])
+    up.append([0,1,0])
+    up.append([0,1,0])
 
-for i in range(sensor_count):
+    laser_dir.append([0, 0, -1])
+    laser_dir.append([0, 0, -1])
+    laser_dir.append([0.70710678, 0.0, -0.70710678])
+    laser_dir.append([0.5, 0.0, -0.866025])    
+    laser_dir.append([0.5, 0.0, -0.866025])
 
-	sensors.append(mi.load_dict({'type': 'perspective',
-				'fov_axis' : 'x',
-				'fov': 10,
-				'to_world': mi.ScalarTransform4f.look_at(
-		    		origin=origin[i],
-		    		target=target[i],
-		    		up=up[i]
-				),
-				'sampler': {
-		    			'type': 'independent',
-		    			'sample_count': spp
-					},
-				'film': {
-		    			'type': 'hdrfilm',
-		    			'width': xRes,
-		    			'height': yRes,
-		    			'rfilter': {
-		        			'type': 'gaussian',
-		        			'stddev' : 0.1,
-		    			},
-		    			'pixel_format': 'rgb',
-	       				},
-	    			}))
+    sensors = []
+    for i in range(len(origin)):
+        sensors.append(mi.load_dict({'type': 'perspective','fov_axis' : 'x','fov': 10,'to_world': mi.ScalarTransform4f.look_at(origin=origin[i],target=target[i],up=up[i]),'sampler': {'type': 'independent','sample_count': ref_sample},'film': {'type': 'hdrfilm','width': 6744,'height': 4502,'rfilter': {'type': 'gaussian','stddev' : 0.1,},'pixel_format': 'rgb',},}))
 
+    return sensors, laser_dir, ref
 
-# LOAD INIT SCENE AND RESAMPLE REF IMAGES
-
-ref_images = [mi.Bitmap(path_to_ref+'synthetic_'+str(i)+'.exr').resample([xRes, yRes]) for i in range(sensor_count)]
-# ref_images = [mi.Bitmap(path_to_ref+'synthetic_'+str(i)+'.exr') for i in range(sensor_count)]
-
-scene = mi.load_file(args.scene_path)
-
-
-# LOAD OPTIMIZER AND INIT AND LOAD OPTIMIZED PARAMETER
+sensors, laser_dir, ref = load_sensors_lights_and_refs("ref/measured")
 
 params = mi.traverse(scene)
 
-key = 'medium1.sigma_t.value.value'
-params[key] = init_sigma_t
-params.update()
-
-opt = mi.ad.Adam(lr=lr)
-opt[key] = params[key]
-params.update(opt)
-
-
-# OPTIMIZATION ---------------------------------------------
-
-loss_evolution = []
-
-total_loss = 0.0
-for it in range(iteration_count):
-    total_loss = 0.0
-    for sensor_idx in range(sensor_count):
-        # Perform the differentiable light transport simulation
-        img1 = mi.render(scene, params, sensor=sensors[sensor_idx], spp=spp, seed=it)
-        img2 = mi.render(scene, params, sensor=sensors[sensor_idx], spp=spp, seed=it+40)
-        
-        # Xi Deng L2 loss function
-        loss = dr.abs(dr.mean((img1 - ref_images[sensor_idx])*(img2 - ref_images[sensor_idx])))
-        
-
-        # classic L2 loss
-        # loss = dr.mean(dr.sqr(img1 - ref_images[sensor_idx]))
-        
-        # Backpropagate gradients
-        dr.backward(loss)
-        
-        # Take a gradient step
-        opt.step()
-        
-        # Clamp the optimized density values. Since we used the `scale` parameter
-        # when instantiating the volume, we are in fact optimizing extinction
-        # in a range from [1e-6 * scale, scale].
-        # opt[key] = dr.clamp(opt[key], 1e-6, 2.0)
-        
-        # Propagate changes to the scene
-        params.update(opt)
-        
-        total_loss += loss[0]
-        print(f"Iteration {it:02d}: Total error={total_loss:6f}, Render {sensor_idx+1:02d}/{sensor_count}: error={loss[0]:6f}", end='\r')
-    loss_evolution.append(total_loss)
-
-print("Final total loss="+str(total_loss))
-
-# FINAL RENDER
-
-final_images = []
-final_ref = []
-final_sub = []
-
-for i in [2,3,5,6,13,31,47]:
-    image = mi.render(scene, sensor=sensors[i], spp=fspp)
-    final_images.append(image)
-    final_ref.append(ref_images[i])
-
-    final_sub.append(img_diff(image, ref_images[i]))
-
-plt.title("Opti sp:"+str(spp)+" Final sp:"+str(fspp)+" Resolution:"+str(xRes)+"*"+str(yRes)+" Nb it:"+str(iteration_count)+" lr:"+str(lr))
-plt.subplot(4, 1, 1)
-plt.plot(range(iteration_count), loss_evolution, label='Loss evolution')
-plt.xlabel("Iteration count")
-plt.ylabel("Loss value")
-
-
-for i in range(len(final_images)):
-    plt.subplot(4, 7, 8+i)
-    plt.imshow(mi.util.convert_to_bitmap(final_ref[i]))
-    plt.xlabel("Sensor "+str(i)+" reference")
-
-    plt.subplot(4, 7, 15+i)
-    plt.imshow(mi.util.convert_to_bitmap(final_images[i]))
-    plt.xlabel("Sensor "+str(i)+" result")
-
-    plt.subplot(4, 7, 22+i)
-    plt.imshow(mi.util.convert_to_bitmap(final_sub[i]))
-    plt.xlabel("Sensor "+str(i)+" difference")
-
-plt.show()
-
-print("sigma_t = "+str(params['medium1.sigma_t.value.value']))
+for i in range(1):
+# for i in range(len(sensors)):
+    params['Laser.direction'] = laser_dir[i]
+    params.update()
+    image = mi.render(scene, spp=ref_sample, sensor=sensors[i])
+    plt.imshow(mi.util.convert_to_bitmap(image))
+    plt.show()
--- utils/getCameraTarget.py
+++ utils/getCameraTarget.py
@@ -1,11 +1,26 @@
 import numpy as np
+import math
 
 def get_target_position(camera_position, direction_vector):
     direction_vector_normalized = direction_vector / np.linalg.norm(direction_vector)
     return camera_position + 1 * direction_vector_normalized
 
+def spherical_to_rectangular_coord(theta, phi):
+    theta = theta * (math.pi / 180)
+    phi = phi * (math.pi / 180)
+    x = math.sin(phi) * math.cos(theta)
+    y = math.sin(phi) * math.sin(theta)
+    z = math.cos(phi)
+    return [x,y,z]
+
+
 print("Target:", get_target_position([.4124579436349758, 0.058085583954402234, -0.33230771606359427], [0.7069253626303624, -0.0050278319727824535, -0.70727028254804]))
+print("Up:", spherical_to_rectangular_coord(45,0))
 print("Target:", get_target_position([-0.26187965706737165, 0.0639193905312978, -0.3371195109510675], [-0.7050955373549855, 0.021399258609251473, -0.7073998022650063]))
+print("Up:", spherical_to_rectangular_coord(45,180))
 print("Target:", get_target_position([0.07629510952287223, 0.06160241046937788, -0.4743865210065597], [0.00010022446694059381, 0.0022486489440108944, -0.99999746676320828]))
+print("Up:", spherical_to_rectangular_coord(0,180))
 print("Target:", get_target_position([-0.04723309843906157, 0.06267489306727239, -0.45902214575013506], [-0.2585864044438378, 0.00788600190483143, -0.9659559422557641]))
+print("Up:", spherical_to_rectangular_coord(15,180))
 print("Target:", get_target_position([0.1995977351808692, 0.06094895876798736, -0.4572594416582021], [0.2587834110929345, -0.0008224308213560734, -0.965935023565586]))
+print("Up:", spherical_to_rectangular_coord(15,180))
